471ce6e4f81eb33b5cb8ffc3e64842597dbbe646
Aleksandr <epicdoomchenko@gmail.com>
2024-03-29 22:59:45 +0300

    Added authorization code example

--- src/AuthorizationServer/AuthorizationServer/Config.cs
+++ src/AuthorizationServer/AuthorizationServer/Config.cs
@@ -60,13 +60,12 @@ public static class Config
             new()
             {
                 ClientId = "interactive.client",
-                ClientSecrets = { new Secret("49C1A7E1-0C79-4A89-A3D6-A37998FB86B0".Sha256()) },
+                ClientSecrets = { new Secret("511536EF-F270-4058-80CA-1C89C192F69A".Sha256()) },
 
                 AllowedGrantTypes = GrantTypes.Code,
+                RequirePkce = false,
 
-                RedirectUris = { "https://localhost:44300/signin-oidc" },
-                FrontChannelLogoutUri = "https://localhost:44300/signout-oidc",
-                PostLogoutRedirectUris = { "https://localhost:44300/signout-callback-oidc" },
+                RedirectUris = { "https://localhost:5002/callback" },
 
                 AllowOfflineAccess = true,
                 AllowedScopes = { "openid", "profile", "authorization_code" }
---
+++ src/Clients/AuthorizationCodeFlowClient.Backend/Program.cs
@@ -0,0 +1,49 @@
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+
+var app = builder.Build();
+
+app.MapGet("/getLink", () =>
+{
+    var link = "https://localhost:5000/connect/authorize"
+               + "?client_id=interactive.client"
+               + "&client_secret=511536EF-F270-4058-80CA-1C89C192F69A"
+               + "&redirect_uri=https://localhost:5002/callback"
+               + "&response_type=code"
+               + "&scope=openid profile authorization_code"
+               + $"&nonce={Guid.NewGuid()}"
+               + $"&state={Guid.NewGuid()}";
+    return Results.Ok(new { Link = link });
+});
+
+app.MapPost("/getToken", async ([FromBody] CodeData codeData, IHttpClientFactory httpClientFactory) =>
+{
+    var body = new FormUrlEncodedContent(new[]
+    {
+        new KeyValuePair<string, string>("grant_type", "authorization_code")
+        ,new KeyValuePair<string, string>("code", codeData.Code)
+        ,new KeyValuePair<string, string>("redirect_uri", "https://localhost:5002/callback")
+        ,new KeyValuePair<string, string>("client_id", "interactive.client")
+        ,new KeyValuePair<string, string>("client_secret", "511536EF-F270-4058-80CA-1C89C192F69A")
+    });
+
+    using var client = httpClientFactory.CreateClient("BackClient");
+    var response = await client.PostAsync("https://localhost:5000/connect/token", body);
+    var responseContent = await response.Content.ReadAsStringAsync();
+    var tokenResponse = JsonSerializer.Deserialize<TokenResponse>(responseContent, new JsonSerializerOptions
+    {
+        PropertyNamingPolicy = JsonNamingPolicy.SnakeCaseLower
+    });
+    
+    
+    return Results.Ok(tokenResponse);
+});
+
+app.Run();
+
+public record CodeData(string Code);
+public record TokenResponse(string AccessToken);
